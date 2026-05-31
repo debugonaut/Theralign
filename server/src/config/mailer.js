e1090a62d7409102f6c60ded284885fab179d5e6
Aadesh <aadeshkhande9202@gmail.com>
2026-05-31 17:47:24 +0530

    fix: force IPv4 for nodemailer to fix Render email ENETUNREACH

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -14,6 +14,7 @@ export const getMailTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       service: 'gmail',
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
